80b425e87bdf20e54d6915ed09d95feb0ac28e93
StefanoTesla <stefano.martini.fi@gmail.com>
2024-10-03 10:04:45 +0200

    client model and database usage

--- app/Services/Alpaca/ClientStatusService.php
+++ app/Services/Alpaca/ClientStatusService.php
@@ -3,62 +3,47 @@
 namespace App\Services\Alpaca;
 
 use Illuminate\Support\Facades\Cache;
+use App\Models\AlpacaClient;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class ClientStatusService{
 
-    static function connect($clientID){
-        if(!Cache::has('alpacaClients')){
-            Cache::put('alpacaClients', []);
-        }
-        $arr = Cache::get('alpacaClients');
+    static function connect(int $clientID,string $device){
 
-        if(!empty($arr)){
-            //cerca se già presente
-            foreach($arr as $key => $client){
-                if($arr[$key]['id'] == $clientID){
-                    $arr[$key]['connected'] = true;
-                    $arr[$key]['time'] = now();
-                    Cache::put('alpacaClients',$arr);
-                    return;
-                }
-            }
-        }
-        //array vuota o id non trovato
-        $newClient = [
-            'id' => $clientID,
-            'connected' => true,
-            'time' => now()
-        ];
-        array_push($arr,$newClient);
-        Cache::put('alpacaClients',$arr);
+        $client = AlpacaClient::where('client_id',$clientID)
+                                ->where('device_type',$device)
+                                ->first();
 
+        if(is_null($client)){
+            $client = new AlpacaClient();
+            $client->client_id = $clientID;
+            $client->connected = true;
+            $client->device_type = $device;
+            $client->save();
+        } else {
+            $client->connected = true;
+            $client->updated_at = now();
+            $client->save();
+        }
     }
 
-    static function disconnect($clientID){
-        if(!Cache::has('alpacaClients')){
-            Cache::put('alpacaClients', []);
-        }
-        $arr = Cache::get('alpacaClients');
-
-        if(!empty($arr)){
-            //cerca se già presente
-            foreach($arr as $key => $client){
-                if($arr[$key]['id'] == $clientID){
-                    $arr[$key]['connected'] = false;
-                    $arr[$key]['time'] = now();
-                    Cache::put('alpacaClients',$arr);
-                    return;
-                }
-            }
+    static function disconnect(int $clientID,string $device){
+        $client = AlpacaClient::where('client_id',$clientID)
+                                ->where('device_type',$device)
+                                ->first();
+        if(!is_null($client)){
+            $client->connected = false;
+            $client->save();
         }
     }
 
     static function list(){
-        return Cache::get('alpacaClients');
+        return AlpacaClient::all()->groupBy('device_type');
     }
 
-    static function destroy(){
-        Cache::put('alpacaClients',[]);
+    static function clean(){
+        AlpacaClient::where('updated_at', '<', Carbon::now()->subDay())->delete();
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 
 
 Route::get('test/', function () {
-
+    ClientStatusService::clean();
     return Response::json(ClientStatusService::list());
 });
 
