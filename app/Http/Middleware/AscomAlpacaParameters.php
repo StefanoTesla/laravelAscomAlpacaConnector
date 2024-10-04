03122b565f92d1ce5b189f9ef1f4f33f12cb73c4
StefanoTesla <stefano.martini.fi@gmail.com>
2024-10-04 10:17:49 +0200

    removed others verbose logs

--- app/Http/Middleware/AscomAlpacaParameters.php
+++ app/Http/Middleware/AscomAlpacaParameters.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use App\Services\Alpaca\ServerTransitionService;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
--- app/Http/Middleware/ReadAlpacaParameters.php
+++ app/Http/Middleware/ReadAlpacaParameters.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -23,7 +22,6 @@ class ReadAlpacaParameters
 
         if(count($bodyParams) > 0){
             foreach($bodyParams as $key => $parameter){
-                Log::info($key ." valore:". $parameter);
                 if(strtolower($key) == 'connected'){
                     $connect = false;
                     if(is_string($parameter) && strtolower($parameter) == 'true'){
